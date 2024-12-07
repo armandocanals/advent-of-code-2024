4202d76ad1815ff5daec0f3320a64414806bc9cc
Armando Canals <armandocanals@gmail.com>
2024-12-07 10:38:10 -0500

    Day 1

---
+++ day1/q1.py
@@ -0,0 +1,14 @@
+f = open("input.txt", "r")
+col1, col2 = [], []
+for i in f:
+    line = i.split(" ")
+    col1.append(line[0])
+    col2.append(line[-1].replace("\n", ""))
+
+col1.sort(), col2.sort()
+distances = []
+
+for j in range(len(col1)):
+    distances.append(abs(int(col1[j]) - int(col2[j])))
+
+print(sum(distances))
---
+++ day1/q2.py
@@ -0,0 +1,16 @@
+f = open("q2-input.txt", "r")
+col1, col2 = [], []
+for i in f:
+    line = i.split(" ")
+    col1.append(line[0])
+    col2.append(line[-1].replace("\n", ""))
+
+res = []
+for i in col1:
+    multiplier = 0
+    for j in col2:
+        if i == j:
+            multiplier += 1
+    res.append(int(i) * multiplier)
+
+print(sum(res))
